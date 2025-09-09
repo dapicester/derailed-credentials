1d4023960210f60bfd1d826863d34af64ad52e1b
Paolo D'Apice <dapicester@gmail.com>
2025-09-09 18:45:45 +0800

    Fix package entrypoint

--- derailed/__main__.py
+++ derailed/__main__.py
@@ -2,7 +2,7 @@
 The main entrypoint, invoke with `derailed` or `python -m derailed`.
 """
 
-if __name__ == "__main__":
-    from derailed.cli import main
+from derailed.cli import main
 
+if __name__ == "__main__":
     main()
