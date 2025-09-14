1bb7a68168d0f3587c3f0d0272ea14183cf4f6b9
Paolo D'Apice <dapicester@gmail.com>
2025-09-14 21:28:10 +0800

    Fix __main__

--- derailed/__main__.py
+++ derailed/__main__.py
@@ -4,5 +4,10 @@ The main entrypoint, invoke with `derailed` or `python -m derailed`.
 
 from derailed.cli import Cli
 
-if __name__ == "__main__":
+
+def main():
     Cli().main()
+
+
+if __name__ == "__main__":
+    main()
